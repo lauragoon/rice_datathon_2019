4448186673be7d20a31ab737dc79332c904d3967
Laura Goon <goon.laura@gmail.com>
2019-01-26 11:51:47 -0600

    lskdjflsd idk what changed

--- emissions_parser.py
+++ emissions_parser.py
@@ -7,11 +7,22 @@ def parse_everything():
     for i in range(len(info)):
         info[i] = info[i].split(",")
         cleaned_data = []
-        cleaned_data.append(info[i][3])
-        cleaned_data.append(info[i][4])
-        cleaned_data.append(info[i][13])
-        cleaned_data.append(info[i][16])
-        cleaned_data.append(info[i][17])
+
+        print info[i][3]
+        print info[i][4]
+        if info[i][3][0] == '"':
+            real_num = int(info[i][3][1:] + info[i][4][:-1])
+            cleaned_data.append(real_num)
+            cleaned_data.append(info[i][13])
+            cleaned_data.append(info[i][16])
+            cleaned_data.append(info[i][17])
+        else:
+            real_num = int(info[i][3])
+            cleaned_data.append(real_num)
+            cleaned_data.append(info[i][12])
+            cleaned_data.append(info[i][15])
+            cleaned_data.append(info[i][16])
+
         info[i] = cleaned_data
 
         # info[i][1] = datetime.strptime(info[i][1][:-13], "%Y-%m-%d")
